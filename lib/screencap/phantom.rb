81bd47acf11dd19f59544121916236dec0080d3a
Dave Spurr <david.spurr@gmail.com>
2013-07-11 14:42:20 +0100

    Raise an error when the page could not be loaded by phantom rather than fail silently

--- lib/screencap/phantom.rb
+++ lib/screencap/phantom.rb
@@ -8,7 +8,8 @@ module Screencap
         output: path
       }.merge(args).collect {|k,v| "#{k}=#{v}"}
       puts RASTERIZE.to_s, params
-      Phantomjs.run(RASTERIZE.to_s, *params)
+      result = Phantomjs.run(RASTERIZE.to_s, *params)
+      raise Screencap::Error, "Could not load URL #{url}" if result.match /Unable to load/
     end
 
     def quoted_args(args)
--- spec/screencap_spec.rb
+++ spec/screencap_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 
 describe Screencap do
   it 'works' do
-    Screencap::Fetcher.new('http://google.com').fetch(output: TMP_DIRECTORY + 'google.png')
+    screenshot = Screencap::Fetcher.new('http://google.com').fetch(output: TMP_DIRECTORY + 'google.png')
+    FastImage.size(screenshot)[0].should == 1024
+  end
+
+  it 'throws error when phantom could not load page' do
+  	expect {
+  		Screencap::Fetcher.new('http://doesnotexistatallipromise.com/').fetch(output: TMP_DIRECTORY + 'foo.png')
+  	}.to raise_error Screencap::Error, "Could not load URL http://doesnotexistatallipromise.com/"
   end
 end
