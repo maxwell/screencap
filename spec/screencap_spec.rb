6404cd4dbb3300dfbaa98a55b91c4c5f56468917
Dave Spurr <david.spurr@gmail.com>
2014-03-18 18:04:53 +0000

    Handle not being able to load URL when loading via frame method

--- spec/screencap_spec.rb
+++ spec/screencap_spec.rb
@@ -7,8 +7,14 @@ describe Screencap do
   end
 
   it 'throws error when phantom could not load page' do
-  	expect {
-  		Screencap::Fetcher.new('http://doesnotexistatallipromise.com/').fetch(output: TMP_DIRECTORY + 'foo.png')
-  	}.to raise_error Screencap::Error, "Could not load URL http://doesnotexistatallipromise.com/"
+    expect {
+      Screencap::Fetcher.new('http://doesnotexistatallipromise.com/').fetch(output: TMP_DIRECTORY + 'foo.png')
+    }.to raise_error Screencap::Error, "Could not load URL http://doesnotexistatallipromise.com/"
+  end
+
+  it 'throws error when phantom could not load page via frame method' do
+    expect {
+      Screencap::Fetcher.new('http://doesnotexistatallipromise.com/').fetch(output: TMP_DIRECTORY + 'foo.png', height: 600)
+    }.to raise_error Screencap::Error, "Could not load URL http://doesnotexistatallipromise.com/"
   end
 end
